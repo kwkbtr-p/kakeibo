7e340d50faf034c004e8463797e543b916eae13b
kwkbtr <other@kwkbtr.info>
2013-03-31 21:03:38 +0900

    initial commit.
    - added Util module
    - added Account class
    - added Transaction class

---
+++ kakeibo.rb
@@ -0,0 +1,75 @@
+require 'date'
+require 'set'
+require 'yaml'
+
+module Kakeibo
+
+  module Util
+
+    def self.loadfile(filename)
+      raise 'invalid filename' unless filename && !filename.empty?
+      open(filename) {|f| YAML.load(f)}
+    end
+
+    def self.savefile(data, filename)
+      raise 'invalid filename' unless filename && !filename.empty?
+      open(filename, 'w') {|f| YAML.dump(data, f)}
+    end
+
+  end
+
+  class Account
+    attr_accessor :filename
+    attr_accessor :name
+
+    def initialize(filename = nil)
+      @filename = filename
+      load if filename && !filename.empty?
+    end
+
+    def load
+      @data = Util::loadfile @filename
+      @name = @data[:name] || @filename.sub(/.yaml$/, '')
+      @totals = @data[:totals] || {}
+      @transactions = @data[:transactions] || {}
+    end
+
+    def set_total(date, total)
+      @totals[Date.parse date] = total
+    end
+
+    def add_transaction(date, transaction)
+      date = Date.parse date
+      t = @transactions[date] || []
+      t << transaction
+      @transactions[date] = t
+    end
+
+    def save
+      @data[:name] = @name
+      @data[:totals] = @totals
+      @data[:transactions] = @transactions
+      Util::savefile @data, @filename
+    end
+
+  end
+
+  class Transaction
+    attr_accessor :amount
+    attr_accessor :title
+    attr_accessor :shop
+    attr_accessor :category
+
+    def initialize(amount = 0,
+                   title = nil,
+                   shop = nil,
+                   category = nil)
+      @amount = amount
+      @title = title
+      @shop = shop
+      @category = category
+    end
+
+  end
+
+end
